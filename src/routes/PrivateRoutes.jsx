d8cfb48d0800050153ac884b6b8473d04da8fd1b
TBence2002 <torokbencee.2002@gmail.com>
2024-02-19 15:56:08 +0100

    private routes

---
+++ src/routes/PrivateRoutes.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Navigate } from 'react-router-dom';
+import { UserAuth } from '../context/AuthContext';
+
+function PrivateRoutes({ children }) {
+    const { currentUser } = UserAuth();
+
+    if (!currentUser) {
+        return <Navigate to="/" replace={true} />
+    }
+
+    return (
+        <>
+            {children}
+        </>
+    )
+}
+
+export default PrivateRoutes
